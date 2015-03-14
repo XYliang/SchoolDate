c7076c1f1ef2bab7400ad72fbb8052a59f63bc09
XYling <492199045@qq.com>
2015-03-14 16:02:30 +0800

    kuangjia

--- SchoolDate/SDAppDelegate.m
+++ SchoolDate/SDAppDelegate.m
@@ -7,14 +7,14 @@
 //
 
 #import "SDAppDelegate.h"
+#import "SDTabBarViewController.h"
 
 @implementation SDAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[SDTabBarViewController alloc]init];
     [self.window makeKeyAndVisible];
     return YES;
 }
