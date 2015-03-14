704407ccfa9d2068632db79e53bdc1824f07dc41
XYling <492199045@qq.com>
2015-03-14 15:49:51 +0800

    Initial Commit

---
+++ SchoolDate/SDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDAppDelegate.h
+//  SchoolDate
+//
+//  Created by xyl on 15-3-14.
+//  Copyright (c) 2015年 cpptd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SchoolDate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SchoolDate
+//
+//  Created by xyl on 15-3-14.
+//  Copyright (c) 2015年 cpptd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
